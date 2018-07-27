87624de3583a3ebb0fc30b4a2075727e31cc3b87
Xiaolin Ge <xiaolinge@Xiaolins-MacBook-Pro.local>
2018-07-27 18:16:34 +1200

    update

--- README.md
+++ README.md
@@ -5,3 +5,33 @@
 ### 相关资料
 
 - [Gatling官网](https://gatling.io/)
+
+
+#########
+
+## Restart Docker
+
+### kill the exit container 
+```aidl
+
+docker ps -l
+
+docker kill fd3c0c622af6
+
+docker rm fd3c0c622af6
+
+```
+
+### 启动数据库
+
+```aidl
+
+  docker run -d \
+  --name postgres \
+  -e POSTGRES_DB=gatling \
+  -e POSTGRES_USER=gatling \
+  -e POSTGRES_PASSWORD=gatling123 \
+  -p 5432:5432 \
+  postgres
+  
+```
--- src/test/java/com/anoyi/test/CollinsonRateTest.scala
+++ src/test/java/com/anoyi/test/CollinsonRateTest.scala
@@ -12,16 +12,16 @@ import scala.concurrent.duration.{Duration, FiniteDuration}
 class CollinsonRateTest extends Simulation {
 
   val scn: ScenarioBuilder = scenario("Rate")
-    .repeat(1000, "n") {
+    .repeat(5, "n") {
     exec(
       http("Rare-API")
-        .get("https://admin.collinsonfxtrader.com/api/v1/rate/real-time?token=eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJmeCIsInN1YiI6ImxlaS56aG91QGNmZ2dsb2JhbC5jby5ueiIsImlhdCI6MTUzMTY1MDAzMiwiZXhwIjoxNTMyNTE0MDMyfQ.vtqqqU0EPOKCuT5wPpUMbDSZ0VRcvvDBmR1ea9J7VuOEXGhxjIsyhA_xCq90JI27rkMiZUAFFKYqcmMFSCEtnQ")
+        .get("http://localhost:8080/product/buy2?id=1")
         .header("Content-Type", "application/json")
         .check(status.is(200))
     ).pause(Duration.apply(1000, TimeUnit.MILLISECONDS))
   }
 
-  setUp(scn.inject(atOnceUsers(20)))
-    .maxDuration(FiniteDuration.apply(10, "minutes"))
+  setUp(scn.inject(atOnceUsers(2)))
+    .maxDuration(FiniteDuration.apply(5, "minutes"))
 
 }
